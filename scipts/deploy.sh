997fd06150aecbdb487d29b8a71bd4e7b89ab269
dev.hupark <dev.hupark@gmail.com>
2023-10-20 12:19:08 +0900

    자동배포 관련 스크립트 및 설정 수정

---
+++ scipts/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=proto-springboot-api
+
+echo "> Build 파일 복사"
+
+cd  $REPOSITORY/zip/*.jar  $REPOSITORY
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+
+echo "> 현재 구동중인 애플리케이션 pid : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+        echo "> run application empty"
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+echo "> jar 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> jar 실행"
+
+nohup java -jar \
+        -Dspring.config.location=classpath:/,file:./config/ \
+        -DSpring.profiles.active=real
+        $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
