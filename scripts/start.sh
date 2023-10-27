bdebf11af0d92ccaa2e36ccf725e3411f48e4f8a
hupark <phwslk@gmail.com>
2023-10-27 16:18:06 +0900

    실행 스크립트 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,6 +27,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/,file:$REPOSITORY/config/ \
+    -Dspring.config.location=classpath:/,file:/home/ec2-user/app/config/ \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
